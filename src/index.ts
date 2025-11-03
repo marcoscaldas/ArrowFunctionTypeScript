2c5d12d18129434821afa9d968eaba3b94c15c98
marcos <marcosdonarumo@gmail.com>
2025-11-03 12:38:13 -0300

    Projeto finalizado com sucesso

--- src/index.ts
+++ src/index.ts
@@ -61,5 +61,123 @@ const adicionarTarefa = (): void =>{
     salvarTarefas(tarefas);
     console.log(`Tarefa  "${descricao}" adicionada! `);
 }
-adicionarTarefa();
+//adicionarTarefa();
 
+
+//LISTAR
+const listarTarefas = ():void =>{
+
+    console.log("\n=== Lista de Tarefas===");
+    if(tarefas.length === 0){
+        console.log('Nenhum tarefa encontrada.');
+        return;
+    }
+
+    tarefas.forEach( (t) =>{
+        console.log(`[${t.concluida ? "X" : " "}] ${t.id} - ${t.descricao}`);
+    });    
+}
+
+//listarTarefas();
+
+
+
+//CONCLUIR
+
+const concluirTarefa = (): void =>{ 
+
+    const id = Number(prompt("Digite o ID da tarefa para concluir: "));
+    const tarefa = tarefas.find((t) => t.id === id);
+
+    if(!tarefa){
+
+        console.log('Tarefa não encontrada! ');
+        return;
+    }
+
+    if(!tarefa.concluida){
+        tarefa.concluida = true;
+
+        salvarTarefas(tarefas);
+        console.log(`Tarefa ${tarefa.descricao} concluída`);
+    }else{
+        console.log(`Tarefa ${tarefa.descricao} já está concluída`);
+    }
+
+}
+//concluirTarefa()
+
+
+//REMOVER
+const removerTarefa = (): void =>{
+
+     const id = Number(prompt("Digite o ID da tarefa para remover: ")); 
+     const antes = tarefas.length;
+     tarefas = tarefas.filter( (t) => t.id !== id);
+
+     if(tarefas.length === antes){
+        console.log('nenhuma terefa foi removida! ')
+     }else{
+
+        salvarTarefas(tarefas);
+        console.log(`Tarefa removida (ID ${id})`);
+     }
+}
+//removerTarefa()
+
+//MENU
+
+const exibirMenu = (): void =>{
+    console.log(`
+    =============================
+        GERENCIADOR DE TAREFAS
+    =============================
+    1 - ADICIONAR
+    2 - LISTAR 
+    3 - CONCLUIR
+    4 - REMOVER
+    0 - SAIR        
+    `);
+}
+//exibirMenu();
+
+const iniciar = (): void =>{
+
+    let opcao: string;
+
+    do{
+        exibirMenu();
+        opcao = prompt('Escolha uma opção: ')
+
+        switch(opcao){
+
+            case "1":
+            adicionarTarefa();
+            break;
+
+            case "2":
+            listarTarefas();
+            break;
+
+            case "3":
+            listarTarefas();
+            concluirTarefa();
+            break;
+
+            case "4":
+            listarTarefas();
+            removerTarefa();
+            break;
+
+            case "0":
+            console.log('Saindo...');
+            break;
+
+            default:
+            console.log('Opção inválida!')
+            break;
+        }        
+    }while (opcao !== '0')
+}
+
+iniciar();
